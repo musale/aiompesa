efd3ce6d16d9976a67c5c648a0d6867ec68dd995
Martin Musale <martinmshale@gmail.com>
2019-06-27 01:23:20 +0300

    Add sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,13 @@
+import sphinx_rtd_theme
+
 project = "aiompesa"
 copyright = "2019, Martin Musale <martinmshale@gmail.com>"
 author = "Martin Musale <martinmshale@gmail.com>"
 extensions = []
 templates_path = ["_templates"]
 exclude_patterns = []
-html_theme = "alabaster"
+
+master_doc = "index"
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
